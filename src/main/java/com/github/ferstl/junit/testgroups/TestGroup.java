ef4850caa70e190a2bcefea3ac02e4221c8f1656
Stefan Ferstl <st.ferstl@gmail.com>
2013-11-13 17:07:39 +0100

    @Inherited

--- src/main/java/com/github/ferstl/junit/testgroups/TestGroup.java
+++ src/main/java/com/github/ferstl/junit/testgroups/TestGroup.java
@@ -2,10 +2,12 @@ package com.github.ferstl.junit.testgroups;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PACKAGE, ElementType.TYPE, ElementType.METHOD})
