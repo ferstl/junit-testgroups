4781c9aa5eb242d50585ddf8f173c8883117b3a6
Stefan Ferstl <st.ferstl@gmail.com>
2016-01-10 00:17:11 +0100

    Refactor

--- src/test/java/DefaultPackageTest.java
+++ src/test/java/DefaultPackageTest.java
@@ -1,12 +1,33 @@
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import com.github.ferstl.junit.testgroups.TestGroup;
 import com.github.ferstl.junit.testgroups.TestGroupRule;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for the default package. Classes from the default package may not be used in other packages.
+ * See {@link http://docs.oracle.com/javase/specs/jls/se7/html/jls-7.html#jls-7.5}.
+ */
 public class DefaultPackageTest {
 
-  @ClassRule
-  public static TestGroupRule rule = new TestGroupRule();
-
   @Test
-  public void test() {}
+  public void classRuleWithoutTestGroupTest() {
+    Result result = JUnitCore.runClasses(ClassRuleWithoutTestGroup.class);
+
+    assertEquals(1, result.getRunCount());
+    assertEquals(0, result.getFailureCount());
+  }
+
+  /**
+   * This class defines the {@link TestGroupRule} but has no {@link TestGroup} annotation.
+   */
+  public static class ClassRuleWithoutTestGroup {
+    @ClassRule
+    public static TestGroupRule rule = new TestGroupRule();
+
+    @Test
+    public void test() {}
+  }
 }
